21cce159a640c977793b805b2c4edc7d77cc3fd0
mosguinz <mos.guinz@gmail.com>
2022-02-10 15:10:03 +0700

    Create class to represent word definition

---
+++ src/Definition.java
@@ -0,0 +1,35 @@
+import java.util.Map;
+
+enum PartOfSpeech {
+    ADJECTIVE, ADVERB, CONJUNCTION, INTERJECTION, NOUN, PREPOSITION, VERB
+}
+
+/**
+ * Represents a definition for a word.
+ * A word may contain multiple definitions.
+ */
+public class Definition implements Map.Entry<PartOfSpeech, String> {
+
+    private final PartOfSpeech partOfSpeech;
+    private final String definition;
+
+    public Definition(PartOfSpeech partOfSpeech, String definition) {
+        this.partOfSpeech = partOfSpeech;
+        this.definition = definition;
+    }
+
+    @Override
+    public PartOfSpeech getKey() {
+        return this.partOfSpeech;
+    }
+
+    @Override
+    public String getValue() {
+        return this.definition;
+    }
+
+    @Override
+    public String setValue(String value) {
+        throw new UnsupportedOperationException();
+    }
+}
