ed7e5d5d2bb7fefeaf0d88d6203139b30eba7b30
mosguinz <mos.guinz@gmail.com>
2022-02-14 18:34:08 +0700

    Add query parameter spec

---
+++ src/QueryParameter.java
@@ -0,0 +1,52 @@
+/**
+ * Represents a query option that the user may provide when making a query.
+ */
+enum QueryOption implements QueryParameter {
+    PART_OF_SPEECH {
+        @Override
+        public QueryOption parse(String s) {
+            try {
+                PartOfSpeech.valueOf(s.toUpperCase());
+                return this;
+            } catch (IllegalArgumentException e) {
+                return null;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "a part of speech";
+        }
+    },
+    DISTINCT,
+    REVERSE;
+
+    @Override
+    public QueryOption parse(String s) {
+        return this.name().equalsIgnoreCase(s) ? this : null;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("'%s'", this.name().toLowerCase());
+    }
+}
+
+interface QueryParameter {
+
+    /**
+     * @return A string representation of the parameter. Displayed to user as part of error
+     *         message.
+     */
+    String toString();
+
+    /**
+     * Get the enum constant of the requested parameter type. Returns {@code null} if the method
+     * fails to parse the given string.
+     *
+     * @return The enum constant that represents a query parameter. Returns {@code null} if the
+     *         specified value cannot be parsed.
+     */
+    QueryParameter parse(String s);
+
+}
