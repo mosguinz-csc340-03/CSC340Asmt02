d0c37aea9a751de14d1e1b2221a5c647d9fc0a3c
mosguinz <mos.guinz@gmail.com>
2022-02-17 04:51:20 +0700

    Add skeleton for query parsing

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
-enum SearchOption {
-    DISTINCT, REVERSE
+abstract class QueryParam {
+
 }
 
 public class Dictionary {
@@ -53,6 +59,64 @@ public class Dictionary {
         do {
             input = dict.promptInput();
         } while (!input.equals("!q"));
+    }
+
+    private Definition[] getEntries(String s) {
+        return entries.get(s.toLowerCase());
+    }
+
+    private DictQuery queryDict(String s) {
+        final String[] args = s.split("\\s");
+        final Queue<QueryOption> paramsToCheck = new LinkedList<>(List.of(QueryOption.values()));
+
+        // CSO 25, abandon query
+        if (args.length > 4) {
+            // invalid
+            return null;
+        }
+
+        if (args[0].equals("!help")) {
+            // print help message
+            return null;
+        }
+
+        Definition[] definitions = getEntries(args[0]);
+
+        if (definitions.length == 0) {
+            // no entry
+            return null;
+        }
+
+        Stream<Definition> stream = Arrays.stream(definitions).sorted();
+
+        for (int i = 1; i < args.length; i++) {
+            String arg = args[i];
+            boolean parsingFailed = true;
+
+            for (QueryOption queryOption : paramsToCheck) {
+                queryOption = queryOption.parse(arg);
+
+                if (queryOption == null) {
+                    continue;
+                }
+
+                parsingFailed = false;
+                stream = switch (queryOption) {
+                    case PART_OF_SPEECH -> {
+                        PartOfSpeech pos = PartOfSpeech.parse(arg);
+                        yield stream.filter(x -> x.partOfSpeech() == pos);
+                    }
+                    case DISTINCT -> stream.distinct();
+                    case REVERSE -> stream.sorted(Collections.reverseOrder());
+                };
+            }
+
+            if (parsingFailed) {
+                // print parsing error message
+            }
+
+        }
+        return null;
     }
 
     /**
